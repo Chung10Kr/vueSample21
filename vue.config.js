1acea9b23dce79ba13e333b26bf857234fb44bb3
Chung10Kr <coohg93@hanmail.net>
2022-02-05 19:50:37 +0900

    [2022-02-05][crlee] - module version modi

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  publicPath: '/',
-  outputDir: 'dist',
 
-  configureWebpack: {
-		resolve: {
-			alias: {
-				'@': path.join(__dirname, 'src/'),
-			},
-		},
-	},
   devServer: {
     port: process.env.VUE_APP_PORT || 8080,
   }
